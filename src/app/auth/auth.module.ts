7760eddd9d19e0241a0903495fed73c4b08cef36
Andre Fortin <andre.v.fortin@gmail.com>
2019-08-19 18:34:40 -0400

    Linting updates for file

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,15 +1,15 @@
-import {ModuleWithProviders, NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {LoginComponent} from './login/login.component';
-import {MatCardModule} from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
-import {RouterModule} from "@angular/router";
-import {ReactiveFormsModule} from "@angular/forms";
-import {MatButtonModule} from "@angular/material/button";
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 import { StoreModule } from '@ngrx/store';
-import {AuthService} from "./auth.service";
+import { AuthService} from './auth.service';
 import * as fromAuth from './auth.reducer';
-import {AuthGuard} from './auth.guard';
+import { AuthGuard } from './auth.guard';
 import { EffectsModule } from '@ngrx/effects';
 import { AuthEffects } from './auth.effects';
 
@@ -21,10 +21,9 @@ import { AuthEffects } from './auth.effects';
         MatCardModule,
         MatInputModule,
         MatButtonModule,
-        RouterModule.forChild([{path: '', component: LoginComponent}]),
+        RouterModule.forChild([{ path: '', component: LoginComponent }]),
         StoreModule.forFeature('auth', fromAuth.authReducer),
         EffectsModule.forFeature([AuthEffects]),
-
     ],
     declarations: [LoginComponent],
     exports: [LoginComponent]
@@ -37,6 +36,6 @@ export class AuthModule {
               AuthService,
               AuthGuard
             ]
-        }
+        };
     }
 }
